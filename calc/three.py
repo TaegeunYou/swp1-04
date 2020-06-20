7caa06e03de31e06476ca35e5461740081610b7e
Taekun <youngryu10@naver.com>
2020-06-20 16:07:41 +0900

    Issue #4 Add file

---
+++ calc/three.py
@@ -0,0 +1,23 @@
+html = b"""
+<html>
+   <body>
+       <form method="get" action="">
+           <p>
+               a = <input type="number" name="a">
+           </p>
+           <p>
+               b = <input type="number" name="b">
+           </p>
+           <p>
+               <input type="submit">
+           </p>
+       </form>
+       <p>
+           a: %(a)s</br>
+           b: %(b)s</br>
+           sum: %(sum)s</br>
+           multiplication: %(multiplication)s</br>
+       </p>
+   </body>
+</html>
+"""
---
+++ calc/two.py
@@ -0,0 +1,30 @@
+from cgi import parse_qs
+from three import html
+
+def application(environ, start_response):
+   d = parse_qs(environ['QUERY_STRING'])
+
+   a = d.get('a', [''])[0]
+   b = d.get('b', [''])[0]
+   q = 0
+   if '' not in [a, b]:
+       a, b = int(a), int(b)
+       for i in range(b):
+           q = q+a
+   sum = a+b
+   multiplication = q
+
+
+   response_body = html % {
+       'a':a,
+       'b':b,
+       'sum':sum,
+       'multiplication':multiplication,
+   }
+
+
+   start_response('200 OK', [
+       ('Content-Type', 'text/html'),
+       ('Content-Length', str(len(response_body)))
+   ])
+   return [response_body]
